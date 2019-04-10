53663d407326fa4f045c315c02ff244bf67ca6ee
kmietotwy <kmietotwy@gmail.com>
2019-04-09 19:18:55 -1000

    PR changes

--- javascript.js
+++ javascript.js
@@ -1,25 +1,19 @@
 function emailCount() {
-  let uniqueEmails;
   let dataset = require('./email_log.json');
   let emails = dataset.emails;
 
-  const getEmails = (function(){ 
-    uniqueEmails = emails.reduce(function (previous, current) {
-      if (!previous.hasOwnProperty(current.email)) {
-        previous[current.email] = 1;
-      } else {
-        previous[current.email] += 1;
-      }
-      return previous;
-    }, {});
-    return uniqueEmails;
-  }())
+  let uniqueEmails = emails.reduce(function (previous, current) {
+    if (!previous.hasOwnProperty(current.email)) {
+      previous[current.email] = 1;
+    } else {
+      previous[current.email] += 1;
+    }
+    return previous;
+  }, {});
 
   return {
-    getEmails: getEmails
+    uniqueEmails
   }
 }
 
 let getUniqueEmails = emailCount();
-
-console.log(getUniqueEmails);
